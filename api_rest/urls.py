4807fb97310cb9a6f1c14d4933c43a8b6e41a4ef
Maria Eduarda Soares <mariaeduardasol2017@gmail.com>
2025-06-05 05:37:32 -0300

    Atualizações no GET

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.get_users, name='get_all_users'),
-    path('user/<str:nick', views.get_by_nick)
+    path('user/<str:nick>', views.get_by_nick),
+    path('data/', views.user_manager)
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -35,3 +35,43 @@ def get_by_nick(request, nick):
 
         serializer = UserSerializer(user)
         return Response(serializer.data)
+    
+#CRUD
+@api_view(['GET','POST','DELETE'])
+def user_manager(request):
+
+    if request.method == 'GET':
+            
+        try:
+            if request.GET['user']:
+
+                user_nickname = request.GET['user']
+
+                try:
+                    user = User.objects.get(pk=user_nickname)
+                except:
+                    return Response(status=status.HTTP_400_BAD_REQUEST)
+                
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+                
+            else: 
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+                
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+# Criando dados
+
+    if request.method == 'POST':
+
+        new_user = request.data
+
+        serializer = UserSerializer(data=new_user)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
