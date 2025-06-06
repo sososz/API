89704fdc88e352453e6259cdfce1f03fce899ebf
Maria Eduarda Soares <mariaeduardasol2017@gmail.com>
2025-06-05 23:48:41 -0300

    INSERÇÃO DE PUT, POST E DELETE

--- api_rest/views.py
+++ api_rest/views.py
@@ -23,7 +23,7 @@ def get_users(request):
     
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def get_by_nick(request, nick):
 
     try:
@@ -36,6 +36,15 @@ def get_by_nick(request, nick):
         serializer = UserSerializer(user)
         return Response(serializer.data)
     
+    if request.method == 'PUT':
+    
+        serializer = UserSerializer(user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 #CRUD
 @api_view(['GET','POST','DELETE'])
 def user_manager(request):
@@ -75,3 +84,55 @@ def user_manager(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+# EDITANDO DADOS
+
+    if request.method == "PUT":
+
+        nickname = request.data['user_nickname']
+
+        update_user = User.objects.get(pk=nickname)
+
+        print(request.data)
+
+        serializer = UserSerializer(updadated_user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+
+# EDITAR DADOS (PUT)
+
+    if request.method == 'PUT':
+
+        nickname = request.data['user_nickname']
+
+        try:
+            update_user = User.objects.get(pk=nickname)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        print(request.data)
+
+        serializer = UserSerializer(update_user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+
+# DELETAR DADOS (DELETE)
+
+    if request.method == 'DELETE':
+
+        try:
+            user_to_delete = User.objects.get(pk=request.data['user_nickname'])
+            user_to_delete.delete()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
